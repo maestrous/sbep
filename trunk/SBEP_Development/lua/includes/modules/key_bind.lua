dec4a5b57ac5735a925798a32f5a59b7ef39f63e
richard.maw <richard.maw@75cd9a46-65cd-11de-b264-b12d837c649a>
2009-08-31 23:27:57 +0000

    Update to key bind. I wish I understood how derma works.

--- trunk/SBEP_Development/lua/includes/modules/key_bind.lua
+++ trunk/SBEP_Development/lua/includes/modules/key_bind.lua
@@ -2,7 +2,7 @@
 local function endmodule()
 	setfenv(2, _G);
 end
-local print = print
+local print, next, _G = print, next, _G
 local tostring, pcall = tostring, pcall;
 local pairs, ipairs = pairs, ipairs
 local CurTime, LocalPlayer, RunConsoleCommand = CurTime, LocalPlayer, RunConsoleCommand
@@ -93,18 +93,6 @@ end
 
 function BindMT:__call(...)
 	return self:IsDown(...)
-end
-
-function BindMT:AddKeyCombination(...)
-	local key_comb
-	if type(select(1,...)) == 'table' then
-		key_comb = select(1,...)
-	else
-		key_comb = {...}
-	end
-	if not rawget(self,'keys') then self.keys = {} end
-
-	return table.insert(self.keys,key_comb)
 end
 
 -- on_change = function(entity, player, bind, down)
@@ -198,6 +186,30 @@ if (CLIENT) then
 		return (CurTime() <= self.down_time) and not self.down;
 	end
 
+	function BindMT:AddKeyCombination(...)
+		local key_comb = {...}
+		if type(key_comb[1]) == 'table' then
+			key_comb = key_comb[1]
+		end
+		if not rawget(self,'keys') then self.keys = {} end
+
+		return table.insert(self.keys,key_comb)
+	end
+
+	function BindMT:RemoveKeyCombination(index)
+		return table.remove(self.keys, index)
+	end
+
+	function BindMT:ReplaceKeyCombination(index, ...)
+		local key_comb = {...}
+		if type(key_comb[1]) == 'table' then
+			key_comb = key_comb[1]
+		end
+		if not rawget(self,'keys') then self.keys = {} end
+
+		self.keys[index] = key_comb
+	end
+
 	hook.Add("Think", "key_bind_Think", function()
 		--print'KeyBind think running'
 		for id, bind in pairs(binds) do
@@ -229,6 +241,167 @@ if (CLIENT) then
 		}
 	end)
 
+end
+
+-- Handy Functions
+-- Keys Iterator, skips non-key enums
+local function keyIterate(state, last)
+	local value
+	while true do
+		local k, v = next(state, last)
+		if not k then return end
+		if k:match'KEY_' then
+			return k,v
+		else
+			last = k
+		end
+	end
+end
+function AllKeys()
+	return keyIterate, _G, nil
+end
+local KeyToEnum = {}
+for enum, key in AllKeys() do
+	KeyToEnum[key] = enum
+end
+local KeyToName = {
+	[1] = "0",
+	[2] = "1",
+	[3] = "2",
+	[4] = "3",
+	[5] = "4",
+	[6] = "5",
+	[7] = "6",
+	[8] = "7",
+	[9] = "8",
+	[10] = "9",
+	[11] = "A",
+	[12] = "B",
+	[13] = "C",
+	[14] = "D",
+	[15] = "E",
+	[16] = "F",
+	[17] = "G",
+	[18] = "H",
+	[19] = "I",
+	[20] = "J",
+	[21] = "K",
+	[22] = "L",
+	[23] = "M",
+	[24] = "N",
+	[25] = "O",
+	[26] = "P",
+	[27] = "Q",
+	[28] = "R",
+	[29] = "S",
+	[30] = "T",
+	[31] = "U",
+	[32] = "V",
+	[33] = "W",
+	[34] = "X",
+	[35] = "Y",
+	[36] = "Z",
+	[37] = "Num 0",
+	[38] = "Num 1",
+	[39] = "Num 2",
+	[40] = "Num 3",
+	[41] = "Num 4",
+	[42] = "Num 5",
+	[43] = "Num 6",
+	[44] = "Num 7",
+	[45] = "Num 8",
+	[46] = "Num 9",
+	[47] = "Num /",
+	[48] = "Num *",
+	[49] = "Num -",
+	[50] = "Num +",
+	[51] = "Num Enter",
+	[52] = "Num .",
+	[53] = "LBRACKET",
+	[54] = "RBRACKET",
+	[55] = ";",
+	[56] = "'",
+	[57] = "`",
+	[58] = ",",
+	[59] = ".",
+	[60] = "/",
+	[61] = "\\",
+	[62] = "-",
+	[63] = "=",
+	[64] = "Enter",
+	[65] = "Space",
+	[66] = "Backspace",
+	[67] = "Tab",
+	[68] = "Caps lock",
+	[69] = "Num lock",
+	[70] = "Escape",
+	[71] = "Scroll Lock",
+	[72] = "Insert",
+	[73] = "Delete",
+	[74] = "Home",
+	[75] = "End",
+	[76] = "Page Up",
+	[77] = "Page Down",
+	[78] = "Break",
+	[79] = "Left Shift",
+	[80] = "Right Shift",
+	[81] = "Left Alt",
+	[82] = "Right Alt",
+	[83] = "Left Control",
+	[84] = "Right Control",
+	[85] = "Left Super",
+	[86] = "Right Super",
+	[87] = "Application",
+	[88] = "Up",
+	[89] = "Left",
+	[90] = "Donw",
+	[91] = "Right",
+	[92] = "F1",
+	[93] = "F2",
+	[94] = "F3",
+	[95] = "F4",
+	[96] = "F5",
+	[97] = "F6",
+	[98] = "F7",
+	[99] = "F8",
+	[100] = "F9",
+	[101] = "F10",
+	[102] = "F11",
+	[103] = "F12",
+--[[	Unusual Keys
+	[104] = "CAPSLOCKTOGGLE",
+	[105] = "NUMLOCKTOGGLE",
+	[106] = "SCROLLLOCKTOGGLE",
+	[107] = "COUNT",
+	[114] = "XBUTTON_A",
+	[115] = "XBUTTON_B",
+	[116] = "XBUTTON_X",
+	[117] = "XBUTTON_Y",
+	[118] = "XBUTTON_LEFT_SHOULDER",
+	[119] = "XBUTTON_RIGHT_SHOULDER",
+	[120] = "XBUTTON_BACK",
+	[121] = "XBUTTON_START",
+	[122] = "XBUTTON_STICK1",
+	[123] = "XBUTTON_STICK2",
+	[153] = "XSTICK1_UP",
+	[146] = "XBUTTON_UP",
+	[147] = "XBUTTON_RIGHT",
+	[155] = "XBUTTON_RTRIGGER",
+	[148] = "XBUTTON_DOWN",
+	[156] = "XSTICK2_RIGHT",
+	[149] = "XBUTTON_LEFT",
+	[157] = "XSTICK2_LEFT",
+	[150] = "XSTICK1_RIGHT",
+	[158] = "XSTICK2_DOWN",
+	[151] = "XSTICK1_LEFT",
+	[159] = "XSTICK2_UP",
+	[152] = "XSTICK1_DOWN",
+	[154] = "XBUTTON_LTRIGGER",
+	[0] = "NONE",
+--]]
+}
+function GetNameFromKey(key)
+	return KeyToName[key]
 end
 
 endmodule()
