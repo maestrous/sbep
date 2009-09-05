2eabdeeff613f2ffbc8b16c1304e1fc53ca0cea1
levybreak098 <levybreak098@75cd9a46-65cd-11de-b264-b12d837c649a>
2009-09-05 16:13:23 +0000

    Fixed to use the new RD3 base entity's name (it was changed from base_rd_entity to base_rd3_entity).

--- SBEP_Entities/lua/entities/base_weapons_module/shared.lua
+++ SBEP_Entities/lua/entities/base_weapons_module/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 		= "anim"
-ENT.Base 		= "base_rd_entity"
+ENT.Base 		= "base_rd3_entity"
 ENT.PrintName 	= "Weapons Module"
 
 list.Set( "LSEntOverlayText" , "base_weapons_module", {num = -1} )
